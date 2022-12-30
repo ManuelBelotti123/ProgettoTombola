424deef4a7a2e44efec4739608b7459774fb0605
manuel <manue@LENOVOPC>
2022-12-30 16:33:48 +0100

    Inserimento del Tabellone e accorgimenti riguardo la generazione delle cartelle

--- ProgettoTombola/Program.cs
+++ ProgettoTombola/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Runtime.Remoting.Messaging;
 using System.Text;
@@ -13,18 +14,11 @@ namespace ProgettoTombola
         static void Main(string[] args)
         {
             //dichiarazione
-            int[,] cart1 = new int[3, 9];
-            //matrice cartella
-            cart1 = GenerazioneCartella(cart1);
-            //stampa della cartella
-            for (int i = 0; i < 3; i++)
-            {
-                Console.SetCursorPosition(0, i);
-                for (int j = 0; j < 9; j++)
-                {
-                    Console.Write(cart1[i, j] + "   ");
-                }
-            }
+            int[,] cart = new int[3, 9], tab = new int[9, 10];
+            //generazione della cartella
+            cart = GenerazioneCartella(cart);
+            //stampa del tabellone
+            tab = TabelloneStampa(tab);
         }
 
         static int[,] GenerazioneCartella(int[,] cartella)
@@ -32,7 +26,6 @@ namespace ProgettoTombola
             //dichiarazione
             Random random = new Random();
             cartella = new int[3, 9];
-            int[,] supporto = new int[3, 9];
             int n = 1, m = 11, sup = 0;
             //generazione della cartella
             for (int i = 0; i < 3; i++)
@@ -42,13 +35,12 @@ namespace ProgettoTombola
                 for (int j = 0; j < 9; j++)
                 {
                     cartella[i, j] = random.Next(n, m);
-                    supporto[i, j] = cartella[i, j];
                     n = n + 10;
                     m = m + 10;
                 }
             }
             //ciclo di modifica dei numeri ripetuti nella cartella
-            
+         
             //ciclo di inserimento delle caselle vuote
             for (int i = 0; i < 3; i++)
             {
@@ -57,7 +49,43 @@ namespace ProgettoTombola
                     cartella[i, j] = 0;
                 }
             }
+            //stampa della cartella
+            for (int i = 0; i < 3; i++)
+            {
+                Console.SetCursorPosition(0, i);
+                for (int j = 0; j < 9; j++)
+                {
+                    Console.Write(cartella[i, j] + "   ");
+                }
+            }
             return cartella;
+        }
+
+        static int[,] TabelloneStampa(int[,] tabellone)
+        {
+            //matrice tabellone
+            tabellone = new int[9, 10];
+            int n = 1, riga = 4;
+            //ciclo per la generazione del tabellone
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    tabellone[i, j] = n;
+                    n++;
+                }
+            }
+            //stampa del tabellone
+            for (int i = 0; i < 9; i++)
+            {
+                Console.SetCursorPosition(0, riga);
+                for (int j = 0; j < 10; j++)
+                {
+                    Console.Write(tabellone[i, j] + "  ");
+                }
+                riga++;
+            }
+            return tabellone;
         }
     }
 }
