c06b8fe15955e07e87b444e7deeab2fbfd7362f0
manuel <manue@LENOVOPC>
2023-01-05 16:14:41 +0100

    Correzione della generazione di una cartella, i numeri in una cartella sono sempre 15 e tutti diversi

--- ProgettoTombola/Program.cs
+++ ProgettoTombola/Program.cs
@@ -16,13 +16,34 @@ namespace ProgettoTombola
         {
             //dichiarazione
             Random random = new Random();
-            int n = 0;
-            int[,] cart = new int[3, 9], tab = new int[9, 10];
+            int n = 0, riga = 0;
+            string[,] cart = new string[3, 9], tab = new string[9, 10];
             //generazione della cartella
             cart = GenerazioneCartella(cart);
+            //stampa della cartella
+            for (int i = 0; i < 3; i++)
+            {
+                Console.SetCursorPosition(0, i);
+                for (int j = 0; j < 9; j++)
+                {
+                    Console.Write(cart[i, j] + "   ");
+                }
+            }
             //stampa del tabellone
-            tab = TabelloneStampa(tab);
+            tab = Tabellone(tab);
+            //stampa del tabellone
+            riga = 6;
+            for (int i = 0; i < 9; i++)
+            {
+                Console.SetCursorPosition(0, riga);
+                for (int j = 0; j < 10; j++)
+                {
+                    Console.Write(tab[i, j] + "  ");
+                }
+                riga++;
+            }
             //estrazione del numero casuale
+            riga = 6;
             while (true)
             {
                 Thread.Sleep(2000);
@@ -30,88 +51,92 @@ namespace ProgettoTombola
                 Console.SetCursorPosition(80, 21);
                 Console.WriteLine(n);
                 //verifica della corrisponenza nelle cartelle
-                VerificaCorrispondenza(cart, tab, n);
+                VerificaCorrispondenza(cart, tab, n, riga);
             }
         }
 
-        static int[,] GenerazioneCartella(int[,] cartella)
+        static string[,] GenerazioneCartella(string[,] cartella)
         {
             //dichiarazione
             Random random = new Random();
-            cartella = new int[3, 9];
+            cartella = new string[3, 9];
             int n = 1, m = 11;
             //generazione della cartella
+            string numerogen = "0";
             for (int i = 0; i < 3; i++)
             {
                 n = 1;
                 m = 11;
+                numerogen = "0";
                 for (int j = 0; j < 9; j++)
                 {
-                    cartella[i, j] = random.Next(n, m);
+                    //generazione di numeri non ripetuti nella cartella
+                    numerogen = random.Next(n, m).ToString();
+                    for (int k = 0; k < 3; k++)
+                    {
+                        for (int l = 0; l < 9; l++)
+                        {
+                            while (cartella[k, l] == numerogen)
+                            {
+                                numerogen = random.Next(n, m).ToString();
+                            }
+                        }
+                    }
+                    //correzione dell'allineamento dei numeri in colonna
+                    if (n == 1 && m == 11 && numerogen != "10")
+                    {
+                        numerogen = " " + numerogen;
+                    }
+                    cartella[i, j] = numerogen;
                     n = n + 10;
                     m = m + 10;
                 }
             }
-            //ciclo di modifica dei numeri ripetuti nella cartella
-         
             //ciclo di inserimento delle caselle vuote
+            int randi = 0;
             for (int i = 0; i < 3; i++)
             {
-                for (int j = random.Next(0, 2); j < 9; j = j + 2)
+                randi = 0;
+                for (int j = 0; j < 4; j++)
                 {
-                    cartella[i, j] = 0;
-                }
-            }
-            //stampa della cartella
-            for (int i = 0; i < 3; i++)
-            {
-                Console.SetCursorPosition(0, i);
-                for (int j = 0; j < 9; j++)
-                {
-                    Console.Write(cartella[i, j] + "   ");
+                    do
+                    {
+                        randi = random.Next(0, 9);
+                    }
+                    while (cartella[i, randi] == "--");
+                    cartella[i, randi] = "--";
                 }
             }
             return cartella;
         }
 
-        static int[,] TabelloneStampa(int[,] tabellone)
+        static string[,] Tabellone(string[,] tabellone)
         {
             //matrice tabellone
-            tabellone = new int[9, 10];
+            tabellone = new string[9, 10];
             int n = 1, riga = 4;
             //ciclo per la generazione del tabellone
             for (int i = 0; i < 9; i++)
             {
                 for (int j = 0; j < 10; j++)
                 {
-                    tabellone[i, j] = n;
+                    tabellone[i, j] = n.ToString();
                     n++;
                 }
-            }
-            //stampa del tabellone
-            for (int i = 0; i < 9; i++)
-            {
-                Console.SetCursorPosition(0, riga);
-                for (int j = 0; j < 10; j++)
-                {
-                    Console.Write(tabellone[i, j] + "  ");
-                }
-                riga++;
             }
             return tabellone;
         }
 
-        static void VerificaCorrispondenza(int[,] cartella, int[,] tabellone, int numero)
+        static void VerificaCorrispondenza(string[,] cartella, string[,] tabellone, int numero, int riga)
         {
             //dichiarazioni
-            int riga = 4;
             //stampa del tabellone
             for (int i = 0; i < 9; i++)
             {
                 Console.SetCursorPosition(0, riga);
                 for (int j = 0; j < 10; j++)
                 {
-                    if (tabellone[i, j] == numero)
+                    if (tabellone[i, j] == numero.ToString())
                     {
                         Console.ForegroundColor = ConsoleColor.Red;
                     }
@@ -126,7 +151,7 @@ namespace ProgettoTombola
                 Console.SetCursorPosition(0, i);
                 for (int j = 0; j < 9; j++)
                 {
-                    if (cartella[i, j] == numero)
+                    if (cartella[i, j] == numero.ToString())
                     {
                         Console.ForegroundColor = ConsoleColor.Red;
                     }
